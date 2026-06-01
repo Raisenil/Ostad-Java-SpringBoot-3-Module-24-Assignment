226de0b2a5b038b3fd0829388b7a4eeaa048ddb0
Raisenil <raisulislamniloy47@gmail.com>
2026-06-01 18:53:54 +0600

    initialize CV Evaluator project with Spring Boot and Maven setup

---
+++ src/test/java/com/example/cv_evaluator/CvEvaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cv_evaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvEvaluatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
