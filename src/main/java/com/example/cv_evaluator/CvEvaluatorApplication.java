4113bdb5009a6a3f8b5009d10c6864aa6cd26491
Raisenil <raisulislamniloy47@gmail.com>
2026-06-01 21:55:05 +0600

    up

--- src/main/java/com/example/cv_evaluator/CvEvaluatorApplication.java
+++ src/main/java/com/example/cv_evaluator/CvEvaluatorApplication.java
@@ -2,8 +2,10 @@ package com.example.cv_evaluator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class CvEvaluatorApplication {
 
     public static void main(String[] args) {
